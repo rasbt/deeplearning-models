56da2dcf6dc5bdd66c5cd41ea9cd5f052ea0e09c
rasbt <mail@sebastianraschka.com>
2022-02-27 20:18:04 -0600

    fix some args

--- templates/pytorch_lightning/submit_command.sh
+++ templates/pytorch_lightning/submit_command.sh
@@ -3,6 +3,6 @@ grid run \
 --framework lightning \
 --gpus 2 \
 tune_classification_basic.py \
---learning_rate "[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05)" \
---batch_size "[64, 128, 256]"\
+--learning_rate "[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05]" \
+--batch_size "[64, 128, 256]" \
 --epochs 20
