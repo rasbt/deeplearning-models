30c6b1a066aec987fe28d31a12970c1a66e61990
sebastian@grid.ai <sebastian@grid.ai>
2022-03-13 22:01:38 +0000

    grouped conv

--- pytorch-lightning_ipynb_new/code_dataset/performance_baseline.py
+++
@@ -1,6 +0,0 @@
-majority_class = test_counter.most_common(1)[0]
-print("Majority class:", majority_class[0])
-
-baseline_acc = majority_class[1] / sum(test_counter.values())
-print("Accuracy when always predicting the majority class:")
-print(f"{baseline_acc:.2f} ({baseline_acc*100:.2f}%)")
